6e76526f97b8db28ad3072466fa5ab40da5420c6
RASHI_BHARTI\rashi <rashibharti.2004@gmail.com>
2026-04-12 13:58:49 +0530

    add file for testing

---
+++ test_vectors.py
@@ -0,0 +1,12 @@
+TEST_VECTORS = [
+    {
+        "plaintext": 0x0000000000000000,
+        "key": 0x00000000000000000000000000000000,
+        "ciphertext": 0xf62bc3ef34f775ac
+    },
+    {
+        "plaintext": 0xfedcba9876543210,
+        "key": 0xfedcba9876543210fedcba9876543210,
+        "ciphertext": 0xc1b71f66160ff587
+    },
+]
